54dc0beeff233aef86b89809d67cce63a6e63c57
Sarthak dhoke <dksarthak101@gmail.com>
2024-12-20 23:10:13 +0530

    done part of JSX

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+   return (
+      <>
+         <div className="container">
+            <form className="items-new">
+               <div className="form-row">
+                  <label htmlFor="item">New Item</label>
+                  <input type="text" id="item" />
+               </div>
+               <button className="btn">Add Item</button>
+            </form>
+            <h1 className="header">Todo List</h1>
+            <ul className="list">
+               <li>
+                  <label>
+                     <input type="checkbox" />
+                     Item 1
+                  </label>
+                  <button className="btn btn-danger">Delete</button>
+               </li>
+            </ul>
+         </div>
+      </>
+   );
+}
+
+export default App;

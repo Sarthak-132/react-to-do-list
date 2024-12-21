be6d21769b1c76ddc18f1180fbfa2c059c5608c9
Sarthak dhoke <dksarthak101@gmail.com>
2024-12-21 10:40:41 +0530

    done to-d-list add new Item

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+   const [newItem, setNewItem] = useState("");
+   const [todos, setTodos] = useState([]);
+
+   function handleSubmit(e) {
+      e.preventDefault();
+
+      setTodos((currentTodos) => {
+         return [
+            ...currentTodos,
+            { id: crypto.randomUUID(), title: newItem, completed: false },
+         ];
+      });
+      setNewItem("");
+   }
+
    return (
       <>
-         <form className="items-new">
+         <form onSubmit={handleSubmit} className="items-new">
             <div className="form-row">
                <label htmlFor="item">New Item</label>
-               <input type="text" id="item" />
+               <input
+                  type="text"
+                  id="item"
+                  value={newItem}
+                  onChange={(e) => setNewItem(e.target.value)}
+               />
             </div>
             <button className="btn">Add Item</button>
          </form>
          <h1 className="header">Todo List</h1>
          <ul className="list">
-            <li>
-               <label>
-                  <input type="checkbox" />
-                  Item 1
-               </label>
-               <button className="btn btn-danger">Delete</button>
-            </li>
+            {todos.map((todo) => {
+               return (
+                  <li key={todo.id}>
+                     <label>
+                        <input type="checkbox" checked={todo.completed} />
+                        {todo.title}
+                     </label>
+                     <button className="btn btn-danger">Delete</button>
+                  </li>
+               );
+            })}
          </ul>
       </>
    );
